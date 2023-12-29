dc5385fbc0deac70a36d9c8cf984861643e22246
Jeremiah Webb <illusion173@hotmail.com>
2023-12-28 22:37:11 -0500

    Lets try this again

--- src/SJ/src/App.jsx
+++ src/SJ/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
