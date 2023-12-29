e5fecc86913276ed0f8f836ce8a6c14527374001
Jeremiah Webb <illusion173@hotmail.com>
2023-12-28 22:29:58 -0500

    Edited Auth component to no longer have sign up functionality

--- src/SJ/src/App.jsx
+++ src/SJ/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+import "./App.css";
+//Front Page
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Please Sign in</h2>
+      <Authenticator loginMechanism={["email"]} hideSignUp={true}>
+        {({ signOut, user }) => (
+          <main>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+      <div>Work in Progress</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/SJ/src/main.jsx
+++ src/SJ/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
